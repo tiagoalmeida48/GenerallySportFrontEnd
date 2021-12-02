f64ccf42db42edd6445bf3b0ddb64066f926269d
Your Name <laisbonesi@gmail.com>
2021-12-01 22:02:16 -0300

    nova-senha

--- assets/js/Ajax/gravarNovaSenha.js
+++ assets/js/Ajax/gravarNovaSenha.js
@@ -1,5 +1,32 @@
 $(function(){
-    $("#btnNovaSenha").click(function(){
-        
-    })
+    var idCliente = sessionStorage.getItem('idCliente');
+
+    $("#btnNovaSenha").click(function(e){
+        e.preventDefault();
+
+        var novaSenha = {
+           // id: parseInt($("#idCliente").val()),
+            id: idCliente,
+            senha: $("#senha").val()      
+        }
+
+
+        $.ajax({
+            url: 'https://localhost:44392/api/Cliente/resetarSenha/',
+            type: "PUT",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            headers: { 'Authorization': 'Bearer ' + token },
+            data: JSON.stringify(novaSenha),
+            success: function(){
+                location.href = 'nova-senha.html';
+            },
+            error: function (err) {
+                console.log(err);
+            },
+            complete: function () {
+            
+            }
+        });
+    });
 });
